750555ecf95504600089339ac89432bd5b376f7b
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-10-22 22:34:58 -0500

    Refactoring whit components

---
+++ 02-react/src/components/JobsSearch.jsx
@@ -0,0 +1,14 @@
+import EmpleosSearchForm from "./EmpleosSearchForm";
+
+function JobsSearch() {
+  return (
+    <section className="jobs-search">
+      <h1>Encuentra tu próximo trabajo</h1>
+      <p>Explora miles de oportunidades en el sector tecnológico.</p>
+
+      <EmpleosSearchForm />
+    </section>
+  );
+}
+
+export default JobsSearch;
