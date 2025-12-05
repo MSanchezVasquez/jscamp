eba8b1f12aa48e0c39271e40a0323af8ce3ce8df
Moises Sanchez <140144292+MSanchezVasquez@users.noreply.github.com>
2025-12-05 18:09:26 -0500

    feat: implement authentication and favorites functionality with routing

--- 03-router-and-zustand/src/components/JobCard.jsx
+++ 03-router-and-zustand/src/components/JobCard.jsx
@@ -1,19 +1,53 @@
 import { useState } from "react";
 import { Link } from "./Link";
 import styles from "./JobCard.module.css";
+import { useFavoritesStore } from "../store/favoritesStore";
+import { useAuthStore } from "../store/authStore";
 
-export function JobCard({ job }) {
+function JobCardFavoriteButton({ jobId }) {
+  const { isLoggedIn } = useAuthStore();
+  // suscríbete a TODA la store y extra TODA la store
+  const { toggleFavorite, isFavorite } = useFavoritesStore();
+
+  return (
+    <button
+      disabled={!isLoggedIn}
+      onClick={() => toggleFavorite(jobId)}
+      aria-label={
+        isFavorite(jobId) ? "Remove from favorites" : "Add to favorites"
+      }
+    >
+      {isFavorite(jobId) ? "❤️" : "🤍"}
+    </button>
+  );
+}
+
+function JobCardApplyButton({ jobId }) {
   const [isApplied, setIsApplied] = useState(false);
-
-  const handleApplyClick = () => {
-    setIsApplied(true);
-  };
+  const { isLoggedIn } = useAuthStore();
 
   const buttonClasses = isApplied
     ? "button-apply-job is-applied"
     : "button-apply-job";
   const buttonText = isApplied ? "Aplicado" : "Aplicar";
 
+  const handleApplyClick = () => {
+    console.log("Aplicando al trabajo con id:", jobId);
+    setIsApplied(true);
+  };
+
+  return (
+    <button
+      disabled={!isLoggedIn}
+      className={buttonClasses}
+      onClick={handleApplyClick}
+    >
+      {buttonText}
+    </button>
+  );
+}
+
+export function JobCard({ job }) {
   return (
     <article
       className="job-listing-card"
@@ -36,9 +70,8 @@ export function JobCard({ job }) {
         <Link href={`/jobs/${job.id}`} className={styles.details}>
           Ver detalles
         </Link>
-        <button className={buttonClasses} onClick={handleApplyClick}>
-          {buttonText}
-        </button>
+        <JobCardApplyButton jobId={job.id} />
+        <JobCardFavoriteButton jobId={job.id} />
       </div>
     </article>
   );
